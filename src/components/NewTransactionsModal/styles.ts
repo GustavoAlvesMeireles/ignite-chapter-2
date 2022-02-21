2c892f227a693ae6b47f73d62f373f69344250c2
Gustavo Alves <guga.alves.m@gmail.com>
2022-02-21 16:44:42 -0300

    transaction api

--- src/components/NewTransactionsModal/styles.ts
+++ src/components/NewTransactionsModal/styles.ts
@@ -1,4 +1,4 @@
-import { darken } from "polished";
+import { darken, transparentize } from "polished";
 import styled from "styled-components";
 
 export const Container = styled.form`
@@ -54,13 +54,28 @@ export const TransactionTypeContainer = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr;
     gap: 0.5rem;
+    
+`;
 
-    button {
-        height: 4rem;
+interface RadioBoxProps {
+    isActive: boolean;
+    activeColor: 'green' | 'red';
+}
+
+const colors = {
+    green: '#33CC95',
+    red: '#E52E4D',
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
+    height: 4rem;
         border: 1px solid #d7d7d7;
         border-radius: 0.25rem;
 
-        background: transparent;
+        background: ${(props) => props.isActive 
+        ? transparentize(0.9, colors[props.activeColor])
+        : 'transparent'
+        };
 
         display: flex;
         align-items: center;
@@ -84,4 +99,4 @@ export const TransactionTypeContainer = styled.div`
             color: var(--text-title);
             }
         }
-    `;
+`;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -68,6 +68,7 @@ export const GlobalStyle = createGlobalStyle`
     .react-modal-content {
         width: 100%;
         max-width: 576px;
+        
         background: var(--background);
         padding: 3rem;
         position: relative;
